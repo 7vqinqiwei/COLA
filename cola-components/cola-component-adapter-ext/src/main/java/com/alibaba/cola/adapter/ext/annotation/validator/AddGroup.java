14a57250d0664fa60a3b06a0e138ccfe336b8b67
??? <waynechin@126.com>
2023-05-25 09:29:01 +0800

    [feature] add default validator group

---
+++ cola-components/cola-component-adapter-ext/src/main/java/com/alibaba/cola/adapter/ext/annotation/validator/AddGroup.java
@@ -0,0 +1,12 @@
+package com.alibaba.cola.adapter.ext.annotation.validator;
+
+import javax.validation.groups.Default;
+
+/**
+ * Add Validator Group
+ * @author qi.wei
+ * @date 2023-05-24
+ */
+public interface AddGroup extends Default {
+
+}
---
+++ cola-components/cola-component-adapter-ext/src/main/java/com/alibaba/cola/adapter/ext/annotation/validator/QueryGroup.java
@@ -0,0 +1,12 @@
+package com.alibaba.cola.adapter.ext.annotation.validator;
+
+import javax.validation.groups.Default;
+
+/**
+ * Query Validator Group
+ * @author qi.wei
+ * @date 2023-05-25
+ */
+public interface QueryGroup extends Default {
+
+}
