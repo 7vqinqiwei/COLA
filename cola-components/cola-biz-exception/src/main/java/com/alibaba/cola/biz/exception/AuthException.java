7201e71e60c6021284c537d4c88aab59625d0879
秦奇威 <waynechin@163.com>
2024-04-11 17:07:22 +0800

    [feat] 新增两个业务异常组件

---
+++ cola-components/cola-biz-exception/src/main/java/com/alibaba/cola/biz/exception/AuthException.java
@@ -0,0 +1,20 @@
+package com.alibaba.cola.biz.exception;
+
+import com.alibaba.cola.exception.BizException;
+
+/**
+ * 认证相关异常
+ *
+ * @author qi.wei
+ * @date 2024/4/11 14:46
+ */
+public class AuthException extends BizException {
+
+    public AuthException() {
+        super(AuthException.class.getSimpleName());
+    }
+
+    public AuthException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
